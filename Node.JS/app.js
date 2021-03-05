3771f668b499feb9efdfb89ee20a531c32ba4bd1
Fiyin <fiyinaregbe@gmail.com>
2021-03-05 12:57:59 -0600

    Created endpoints

--- Node.JS/app.js
+++ Node.JS/app.js
@@ -1,10 +1,104 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+var fs = require('fs'); //require file system object
 
-app.get('/health', (req, res) => {
-   res.send('You keep using that word. I do not think it means what you think it means.');
-});
+var customers = {
+   customer1:{ 
+      firstName: "Carol",
+      lastName: "Danvers",
+      age: 33,
+      address:
+      {
+          streetAddress: "417 5th Avenue Apt 10B",
+          city: "New York",
+          state: "NY",
+          postalCode: "10016"
+      },
+      phoneNumber: "2125551234",
+      id: 1,
+  },
+  customer2:{
+      firstName: "Tony",
+      lastName: "Stark",
+      age: 48,
+      address:
+      {
+          streetAddress: "890 Fifth Avenue",
+          city: "New York",
+          state: "NY",
+          postalCode: "10021"
+      },
+      phoneNumber: "2125554567"
+  },
+  customer3:{
+      firstName: "Steve",
+      lastName: "Rogers",
+      age: 93,
+      address:
+      {
+          streetAddress: "890 Fifth Avenue",
+          city: "New York",
+          state: "NY",
+          postalCode: "10021"
+      },
+      phoneNumber: "6781367092"
+  }
+}
+
+var orders =  {
+  customer1:{
+      order: "phone",
+      id: 1
+   },
+   customer2:{
+      order: "books",
+      id: 2
+   },
+   customer3:{
+       order: "car",
+       id: 3
+   }
+  }
+
+
+app.get('/health/customer', (req, res) => {
+  fs.readFile(__dirname + "/" + "customers.json",  function(err, data){
+    console.log(data);
+    res.send(orders);
+  });
+})
+
+//The addOrder endpoint
+app.post('/health/customer/:id', function(req, res) {
+  var id = parseInt(req.params.id);
+	var newCustomer = req.body;
+    orders["customer" + id] = newCustomer;
+	  console.log("--->After Post, customer orders:\n" + JSON.stringify(orders, null, 4));
+    res.end("Post Successfully: \n" + JSON.stringify(newCustomer, null, 4));
+})
+
+    // Update a Customer Order with Id
+  app.put('/health/customer/:id', function(req, res) {
+    var id = parseInt(req.params.id);
+    var updatedCustomer = req.body; 
+    if(orders["customer" + id] != null){
+      // update data
+      orders["customer" + id] = updatedCustomer;
+  
+      console.log("--->Update Successfully,  customer orders: \n" + JSON.stringify(orders, null, 4))}
+    });
+
+
+   // Delete a Customer Order with Id
+   app.delete('/health/customer/:id', function(req, res) {
+    var deleteCustomer = orders["customer" + req.params.id];
+      delete orders["customer" + req.params.id];
+      console.log("--->After deletion, customer orders:\n" + JSON.stringify(orders, null, 4) );
+      res.end( "Deleted customer order: \n" + JSON.stringify(deleteCustomer, null, 4));
+  } );
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
